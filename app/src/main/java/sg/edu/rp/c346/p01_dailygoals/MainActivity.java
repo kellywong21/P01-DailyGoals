69267a3cb589d8d74531ca587f6613cb01654c56
18004885 <18004885@myrp.edu.sg>
2020-04-24 14:48:09 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/p01_dailygoals/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.p01_dailygoals;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.IntentCompat;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}

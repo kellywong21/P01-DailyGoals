76fdd0cb89aaf976b4bb459d1eeeb8a971085e6f
18004885 <18004885@myrp.edu.sg>
2020-04-24 14:52:06 +0800

    FINAL

--- app/src/main/java/sg/edu/rp/c346/p01_dailygoals/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p01_dailygoals/MainActivity.java
@@ -18,7 +18,11 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    RadioGroup rg1,rg2,rg3;
+    EditText etReflection;
+    Button btnOK;
+    RadioButton rb1,rb2,rb3;
+    int selectedButtonId1,selectedButtonId2,selectedButtonId3;
 
 
 
@@ -26,7 +30,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rg1 = findViewById(R.id.rg2);
+        rg2 = findViewById(R.id.rg2);
+        rg3 = findViewById(R.id.rg3);
+        etReflection = findViewById(R.id.etReflection);
+        btnOK = findViewById(R.id.btnOk);
 
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String reflection = etReflection.getText().toString();
+                selectedButtonId1 = rg1.getCheckedRadioButtonId();
+                selectedButtonId2 = rg2.getCheckedRadioButtonId();
+                selectedButtonId3 = rg3.getCheckedRadioButtonId();
+
+                rb1 = findViewById(selectedButtonId1);
+                rb2 = findViewById(selectedButtonId2);
+                rb3 = findViewById(selectedButtonId3);
+
+
+
+                String[] summary = {rb1.getText().toString(),rb2.getText().toString(),rb3.getText().toString(),reflection};
+
+                Intent intent = new Intent(MainActivity.this,Summary.class);
+                intent.putExtra("summary",summary);
+                startActivity(intent);
+
+
+            }
+        });
 
     }
 
--- app/src/main/java/sg/edu/rp/c346/p01_dailygoals/Summary.java
+++ app/src/main/java/sg/edu/rp/c346/p01_dailygoals/Summary.java
@@ -10,14 +10,31 @@ import android.widget.TextView;
 
 public class Summary extends AppCompatActivity {
 
-
+    TextView tv1,tv2,tv3,tv4;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        tv4 = findViewById(R.id.tv4);
+        btnBack = findViewById(R.id.btnBack);
 
+        Intent intent = getIntent();
+        String [] summary = intent.getStringArrayExtra("summary");
+        tv1.setText("Read up on material before class: " + summary[0]);
+        tv2.setText("Arrive on time so as not to miss important part of lesson: " + summary[1]);
+        tv3.setText("Attempt the problem myself: " + summary[2]);
+        tv4.setText("Reflection: " + summary[3]);
 
-        
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Summary.this,MainActivity.class));
+            }
+        });
     }
 }
